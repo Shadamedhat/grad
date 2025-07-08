31d56db77841774fdd7d4f511d9500b2febe9027
Shadamedhat <shadamedhat12@gmail.com>
2025-07-09 00:30:29 +0300

    fix: remove duplicated /api from blueprint routes

--- flask_dashboard/routes/dashboard_routes.py
+++ flask_dashboard/routes/dashboard_routes.py
@@ -3,7 +3,7 @@ from controllers.dashboard_controller import *
 
 dashboard_bp = Blueprint('dashboard_bp', __name__)
 
-dashboard_bp.route('/api/top_active_students', methods=['GET'])(get_top_active_students)
-dashboard_bp.route('/api/ratings_distribution', methods=['GET'])(get_ratings_distribution)
-dashboard_bp.route('/api/appointments_status', methods=['GET'])(get_appointments_status)
-dashboard_bp.route('/api/treatment_orders_by_month', methods=['GET'])(get_treatment_orders_by_month)
+dashboard_bp.route('/top_active_students', methods=['GET'])(get_top_active_students)
+dashboard_bp.route('/ratings_distribution', methods=['GET'])(get_ratings_distribution)
+dashboard_bp.route('/appointments_status', methods=['GET'])(get_appointments_status)
+dashboard_bp.route('/treatment_orders_by_month', methods=['GET'])(get_treatment_orders_by_month)
